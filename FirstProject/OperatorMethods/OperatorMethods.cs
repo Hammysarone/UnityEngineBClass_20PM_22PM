c7f49b53ded0a2f7ce58421722e2ff5f7d933871
User <master19588@gmail.com>
2022-06-21 21:50:20 +0900

    Learned Operators
    Added OperatorMethod
    Added Operators

---
+++ FirstProject/OperatorMethods/OperatorMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorMethods
+{
+    internal class OperatorMethods
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static float Sum(float a, float b)
+        {
+            return a + b;
+        }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
