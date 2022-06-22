a9d8220e78ad5e5451d23b6f600ade3b2ae92607
User <master19588@gmail.com>
2022-06-22 21:50:58 +0900

    Learned Array
    Added Project "Array"

--- FirstProject/OperatorMethods/OperatorMethods.cs
+++ FirstProject/OperatorMethods/OperatorMethods.cs
@@ -8,10 +8,13 @@ namespace OperatorMethods
 {
     internal class OperatorMethods
     {
-        public static int Sum(int a, int b)
+        public static int Sum(int a, int b) //매개변수도 지역변수
         {
             return a + b;
         }
+        // 함수 오버로딩
+        // 같은 기능을 수행하는 함수의 이름을 똑같이 하면서
+        // 파라미터가 다르면 동일한 이름의 함수도 여러개 정의할 수 있는 기능
         public static float Sum(float a, float b)
         {
             return a + b;
@@ -19,6 +22,123 @@ namespace OperatorMethods
         public static int Sub(int a, int b)
         {
             return a - b;
+        }
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+        public static int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Mod(int a, int b)
+        {
+            return a % b;
+        }
+        public static int Increase(int a)
+        {
+            return ++a;
+        }
+        public static int Decrease(int a)
+        {
+            return --a;
+        }
+        public static bool IsSame(int a, int b)
+        {
+            return a == b;
+        }
+        public static bool IsDifferent(int a, int b)
+        {
+            return a != b;
+        }
+        public static bool IsBigger(int a, int b)
+        {
+            return a > b;
+        }
+        public static bool IsSmaller(int a, int b)
+        {
+            return a < b;
+        }
+        public static bool IsBiggerOrSame(int a, int b)
+        {
+            return a >= b;
+        }
+        public static bool IsSmallerOrSame(int a, int b)
+        {
+            return a <= b;
+        }
+        public static int PlusBToA(int a, int b)
+        {
+            a += b;
+            return a;
+        }
+        public static int MinusBToA(int a, int b)
+        {
+            a -= b;
+            return a;
+        }
+        public static int MultiplyBToA(int a, int b)
+        {
+            a *= b;
+            return a;
+        }
+        public static int DivideBToA(int a, int b)
+        {
+            a /= b;
+            return a;
+        }
+        public static int ModBToA(int a, int b)
+        {
+            a %= b;
+            return a;
+        }
+        public static bool LogicOr(bool a, bool b)
+        {
+            return a | b;
+        }
+        public static bool LogicAnd(bool a, bool b)
+        {
+            return a & b;
+        }
+        public static bool LogicXOR(bool a, bool b)
+        {
+            return a ^ b;
+        }
+        public static bool LogicNOT(bool a)
+        {
+            return !a;
+        }
+        public static bool ConditionalLogicOr(bool a, bool b)
+        {
+            return a || b;
+        }
+        public static bool ConditionalLogicAND(bool a, bool b)
+        {
+            return a && b;
+        }
+        static public int BitLogicOR(int a, int b)
+        {
+            return a | b;
+        }
+        static public int BitLogicAND(int a, int b)
+        {
+            return a & b;
+        }
+        static public int BitLogicXOR(int a, int b)
+        {
+            return a ^ b;
+        }
+        static public int BitShiftLeft(int a, int howManyBitYouWantToShift)
+        {
+            return a >> howManyBitYouWantToShift;
+        }
+        static public int BitShiftRight(int a, int howManyBitYouWantToShift)
+        {
+            return a >> howManyBitYouWantToShift;
+        }
+        static public int BitComplement(int a)
+        {
+            return ~a;
         }
     }
 }
--- FirstProject/OperatorMethods/Program.cs
+++ FirstProject/OperatorMethods/Program.cs
@@ -7,6 +7,11 @@ namespace OperatorMethods
         static void Main(string[] args)
         {
             Console.WriteLine(OperatorMethods.Sum(1, 2));
+            Console.WriteLine(OperatorMethods.Sum(2, 3));
+            Console.WriteLine(OperatorMethods.Sum(3, 4));
+            Console.WriteLine(OperatorMethods.Sum(4, 5));
+            // 디버그 중 F10 프로시저 단위로 실행
+            // 디버그 중 F11 코드 단위로 실행
         }
     }
 }
