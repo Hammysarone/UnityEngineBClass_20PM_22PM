11cc8aef52ba21935fc935e9d3e4f4f6cededb24
User <master19588@gmail.com>
2022-08-12 21:48:46 +0900

    Platformer2D - EFFECTOR COLLIDER, Slide, ATTACK, and Dash
    satt

--- Platformer2D/Assets/02Scripts/PlayerController.cs
+++ Platformer2D/Assets/02Scripts/PlayerController.cs
@@ -60,17 +60,42 @@ public class PlayerController : MonoBehaviour
         Finish
     }
 
+    private enum AttackState
+    {
+        Idle,
+        Prepare,
+        Casting,
+        OnAction,
+        Finish
+    }
+
+    private enum DashState
+    {
+        Idle,
+        Prepare,
+        Casting,
+        OnAction,
+        Finish
+    }
+
     public State state;
-    [SerializeField] private MoveState  _moveState;
-    [SerializeField] private IdleState  _idleState;
-    [SerializeField] private JumpState  _jumpState;
-    [SerializeField] private FallState  _fallState;
-    [SerializeField] private SlideState _slideState;
+    [SerializeField] private MoveState   _moveState;
+    [SerializeField] private IdleState   _idleState;
+    [SerializeField] private JumpState   _jumpState;
+    [SerializeField] private FallState   _fallState;
+    [SerializeField] private SlideState  _slideState;
+    [SerializeField] private AttackState _attackState;
+    [SerializeField] private DashState   _dashState;
 
     private Vector2 _move;
     [SerializeField] private float _moveSpeed  = 1.0f;
     [SerializeField] private float _jumpForce  = 5.0f;
     [SerializeField] private float _slideSpeed = 3.0f;
+    [SerializeField] private float _dashSpeed  = 4.0f;
+
+    [SerializeField] private Vector2 _attackHitCastCenter;
+    [SerializeField] private Vector2 _attackHitCastSize;
+    [SerializeField] private LayerMask _attackTargetLayer;
 
     private int _direction;
 
@@ -100,9 +125,16 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D _rb;
     private CapsuleCollider2D _col;
     private GroundDetecter _groundDetecter;
+    private Vector2 _colSizeOrigin;
+    private Vector2 _colOffsetOrigin;
+    [SerializeField] private Vector2 _colOffsetCrouch;
+    [SerializeField] private Vector2 _colSizeCrouch;
 
     private bool isMovable = true;
+    private bool isDirenctionChangable = true;
     private float _slideAnimationTime;
+    private float _attackAnimationTime;
+    private float _dashAnimationTime;
     private float _animationTimer;
 
     private float h { get => Input.GetAxis("Horizontal"); }
@@ -115,15 +147,23 @@ public class PlayerController : MonoBehaviour
         _col = GetComponent<CapsuleCollider2D>();
         _animator = GetComponent<Animator>();
         _groundDetecter = GetComponent<GroundDetecter>();
+        _colOffsetOrigin = _col.offset;
+        _colSizeOrigin = _col.size;
         _slideAnimationTime = GetAnimationTime("Slide");
+        _attackAnimationTime = GetAnimationTime("Attack");
+        _dashAnimationTime = GetAnimationTime("Dash");
     }
 
     private void Update()
     {
-        if (h < 0.0f)
-            direction = -1;
-        else if (h > 0.0f)
-            direction = 1;
+        if(isDirenctionChangable)
+        { 
+            if (h < 0.0f)
+                direction = -1;
+            else if (h > 0.0f)
+                direction = 1;
+        }
+
         
         if(isMovable)
         {
@@ -134,6 +174,7 @@ public class PlayerController : MonoBehaviour
                 ChangeState(State.Idle);
         }
 
+        // 점프
         if (Input.GetKeyDown(KeyCode.Z) && 
             state != State.Jump &&
             state != State.Fall)
@@ -141,10 +182,35 @@ public class PlayerController : MonoBehaviour
             ChangeState(State.Jump);
         }
 
-        if (Input.GetKeyDown(KeyCode.C))
-            ChangeState(State.Slide);
+        // 슬라이드
+        if (Input.GetKeyDown(KeyCode.LeftControl) &&
+            (state == State.Idle || state == State.Move))
+        {
 
-        UpdateState();
+            ChangeState(State.Slide);
+        }
+
+        // 공격
+        if (Input.GetKeyDown(KeyCode.X) &&
+            (state == State.Idle ||
+             state == State.Move ||
+             state == State.Jump ||
+             state == State.Fall))
+        {
+            ChangeState(State.Attack);
+        }
+
+        // 대쉬
+        if(Input.GetKeyDown(KeyCode.C) &&
+            (state == State.Idle ||
+            state == State.Move ||
+            state == State.Jump ||
+            state == State.Fall))
+        {
+            ChangeState(State.Dash);
+        }
+
+            UpdateState();
     }
 
     private void FixedUpdate()
@@ -169,8 +235,10 @@ public class PlayerController : MonoBehaviour
                 UpdateFallState();
                 break;
             case State.Attack:
+                UpdateAttackState();
                 break;
             case State.Dash:
+                UpdateDashState();
                 break;
             case State.Slide:
                 UpdateSlideState();
@@ -201,11 +269,15 @@ public class PlayerController : MonoBehaviour
                 _fallState = FallState.Idle;
                 break;
             case State.Attack:
+                _attackState = AttackState.Idle;
                 break;
             case State.Dash:
+                _dashState = DashState.Idle;
                 break;
             case State.Slide:
                 _slideState = SlideState.Idle;
+                _col.offset = _colOffsetOrigin;
+                _col.size = _colSizeOrigin;
                 break;
             default:
                 break;
@@ -227,11 +299,15 @@ public class PlayerController : MonoBehaviour
                 _fallState = FallState.Prepare;
                 break;
             case State.Attack:
+                _attackState = AttackState.Prepare;
                 break;
             case State.Dash:
+                _dashState = DashState.Prepare;
                 break;
             case State.Slide:
                 _slideState = SlideState.Prepare;
+                _col.offset = _colOffsetCrouch;
+                _col.size = _colSizeCrouch;
                 break;
             default:
                 break;
@@ -248,6 +324,7 @@ public class PlayerController : MonoBehaviour
                 break;
             case IdleState.Prepare:
                 isMovable = true;
+                isDirenctionChangable = true;
                 _animator.Play("Idle");
                 _idleState = IdleState.OnAction;
                 break;
@@ -271,6 +348,7 @@ public class PlayerController : MonoBehaviour
                 break;
             case MoveState.Prepare:
                 isMovable = true;
+                isDirenctionChangable = true;
                 _animator.Play("Move");
                 _moveState = MoveState.OnAction;
                 break;
@@ -294,7 +372,9 @@ public class PlayerController : MonoBehaviour
                 break;
             case JumpState.Prepare:
                 isMovable = false;
+                isDirenctionChangable = true;
                 _animator.Play("Jump");
+                _rb.velocity = new Vector2(_rb.velocity.x, 0.0f);
                 _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
                 _jumpState++;
                 break;
@@ -324,6 +404,7 @@ public class PlayerController : MonoBehaviour
                 break;
             case FallState.Prepare:
                 isMovable = false;
+                isDirenctionChangable = true;
                 _animator.Play("Fall");
                 _fallState = FallState.OnAction;
                 break;
@@ -350,12 +431,13 @@ public class PlayerController : MonoBehaviour
                 break;
             case SlideState.Prepare:
                 isMovable = false;
+                isDirenctionChangable = false;
                 _animator.Play("Slide");
                 _animationTimer = _slideAnimationTime;
                 _slideState++;
                 break;
             case SlideState.Casting:
-                if (_animationTimer < _slideAnimationTime * 3 / 4)
+                if (_animationTimer < _slideAnimationTime * 3.0f / 4.0f)
                     _slideState++;
                 else
                 {
@@ -364,7 +446,7 @@ public class PlayerController : MonoBehaviour
                 _animationTimer -= Time.deltaTime;
                 break;
             case SlideState.OnAction:
-                if (_animationTimer < _slideAnimationTime * 1 / 4)
+                if (_animationTimer < _slideAnimationTime * 1.0f / 4.0f)
                     _slideState++;
                 else
                 {
@@ -388,6 +470,82 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void UpdateAttackState()
+    {
+        switch (_attackState)
+        {
+            case AttackState.Idle:
+                break;
+            case AttackState.Prepare:
+                isMovable = false;
+                isDirenctionChangable = false;
+                _animator.Play("Attack");
+                _animationTimer = _attackAnimationTime;
+                _attackState = AttackState.OnAction;
+                break;
+            case AttackState.Casting:
+                break;
+            case AttackState.OnAction:
+                if(_animationTimer < 0)
+                {
+                    ChangeState(State.Idle);
+                }
+                _animationTimer -= Time.deltaTime;
+                break;
+            case AttackState.Finish:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void UpdateDashState()
+    {
+        switch (_dashState)
+        {
+            case DashState.Idle:
+                break;
+            case DashState.Prepare:
+                isMovable = false;
+                isDirenctionChangable = false;
+                _animator.Play("Dash");
+                _animationTimer = _dashAnimationTime;
+                _dashState++;
+                break;
+            case DashState.Casting:
+                if (_animationTimer < _dashAnimationTime * 3.5f / 4.0f)
+                    _dashState++;
+                else
+                {
+                    _rb.velocity = Vector2.right * direction * _moveSpeed;
+                }
+                _animationTimer -= Time.deltaTime;
+                break;
+            case DashState.OnAction:
+                if (_animationTimer < _dashAnimationTime * 1.5f / 4.0f)
+                    _dashState++;
+                else
+                {
+                    _rb.velocity = Vector2.right * _direction * _dashSpeed;
+                }
+                _animationTimer -= Time.deltaTime;
+                break;
+            case DashState.Finish:
+                if (_animationTimer < 0.0f)
+                {
+                    ChangeState(State.Idle);
+                }
+                else
+                {
+                    _rb.velocity = Vector2.right * _direction * _moveSpeed;
+                }
+                _animationTimer -= Time.deltaTime;
+                break;
+            default:
+                break;
+        }
+    }
+
     private float GetAnimationTime(string clipName)
     {
         RuntimeAnimatorController rac = _animator.runtimeAnimatorController;
@@ -401,5 +559,34 @@ public class PlayerController : MonoBehaviour
 
         Debug.Log($"{clipName}을 찾지 못함");
         return -1.0f;
+    }
+
+    private void AttackHit()
+    {
+        Vector2 attackCenter = new Vector2(_attackHitCastCenter.x * _direction,
+                                           _attackHitCastCenter.y) + _rb.position;
+
+        RaycastHit2D hit = Physics2D.BoxCast(attackCenter,
+                          _attackHitCastSize,
+                          0.0f,
+                          Vector2.zero, 
+                          0.0f, 
+                          _attackTargetLayer);
+
+        if(hit.collider != null)
+        {
+            Debug.Log($"Attack Hit ! : {hit.collider.gameObject.name}");
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_rb == null)
+            return;
+
+        Vector2 attackCenter = new Vector2(_attackHitCastCenter.x * _direction,
+                                           _attackHitCastCenter.y) + _rb.position;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(attackCenter, _attackHitCastSize);
     }
 }
