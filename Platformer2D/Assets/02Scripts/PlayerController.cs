31d5858b482c24d508fc9c8ce90bcba00ee84a8d
User <master19588@gmail.com>
2022-08-16 21:52:58 +0900

    Platformer2D - Crouch, DownJump
    satt

--- Platformer2D/Assets/02Scripts/PlayerController.cs
+++ Platformer2D/Assets/02Scripts/PlayerController.cs
@@ -12,7 +12,9 @@ public class PlayerController : MonoBehaviour
         Fall,
         Attack,
         Dash,
-        Slide
+        Slide,
+        Crouch,
+        DownJump
     }
 
     private enum IdleState
@@ -78,20 +80,41 @@ public class PlayerController : MonoBehaviour
         Finish
     }
 
+    private enum CrouchState
+    {
+        Idle,
+        Prepare,
+        Casting,
+        OnAction,
+        Finish
+    }
+
+    private enum DownJumpState
+    {
+        Idle,
+        Prepare,
+        Casting,
+        OnAction,
+        Finish
+    }
+
     public State state;
-    [SerializeField] private MoveState   _moveState;
-    [SerializeField] private IdleState   _idleState;
-    [SerializeField] private JumpState   _jumpState;
-    [SerializeField] private FallState   _fallState;
-    [SerializeField] private SlideState  _slideState;
-    [SerializeField] private AttackState _attackState;
-    [SerializeField] private DashState   _dashState;
+    [SerializeField] private MoveState     _moveState;
+    [SerializeField] private IdleState     _idleState;
+    [SerializeField] private JumpState     _jumpState;
+    [SerializeField] private FallState     _fallState;
+    [SerializeField] private SlideState    _slideState;
+    [SerializeField] private AttackState   _attackState;
+    [SerializeField] private DashState     _dashState;
+    [SerializeField] private CrouchState   _crouchState;
+    [SerializeField] private DownJumpState _downJumpState;
 
     private Vector2 _move;
-    [SerializeField] private float _moveSpeed  = 1.0f;
-    [SerializeField] private float _jumpForce  = 5.0f;
-    [SerializeField] private float _slideSpeed = 3.0f;
-    [SerializeField] private float _dashSpeed  = 4.0f;
+    [SerializeField] private float _moveSpeed     = 1.0f;
+    [SerializeField] private float _jumpForce     = 5.0f;
+    [SerializeField] private float _slideSpeed    = 3.0f;
+    [SerializeField] private float _dashSpeed     = 4.0f;
+    [SerializeField] private float _downJumpForce = 1.0f;
 
     [SerializeField] private Vector2 _attackHitCastCenter;
     [SerializeField] private Vector2 _attackHitCastSize;
@@ -175,18 +198,27 @@ public class PlayerController : MonoBehaviour
         }
 
         // 점프
-        if (Input.GetKeyDown(KeyCode.Z) && 
-            state != State.Jump &&
-            state != State.Fall)
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            ChangeState(State.Jump);
+            if(state != State.Crouch)
+            {
+                if (state != State.Jump &&
+                    state != State.Fall &&
+                    state != State.DownJump &&
+                    state != State.Crouch)
+                    ChangeState(State.Jump);
+            }
+            else
+            {
+                ChangeState(State.DownJump);
+            }
+
         }
 
         // 슬라이드
         if (Input.GetKeyDown(KeyCode.LeftControl) &&
             (state == State.Idle || state == State.Move))
         {
-
             ChangeState(State.Slide);
         }
 
@@ -208,6 +240,14 @@ public class PlayerController : MonoBehaviour
             state == State.Fall))
         {
             ChangeState(State.Dash);
+        }
+
+        // 숙이기
+        if(Input.GetKey(KeyCode.DownArrow) &&
+            (state == State.Idle ||
+             state == State.Move))
+        {
+            ChangeState(State.Crouch);
         }
 
             UpdateState();
@@ -242,6 +282,12 @@ public class PlayerController : MonoBehaviour
                 break;
             case State.Slide:
                 UpdateSlideState();
+                break;
+            case State.Crouch:
+                UpdateCrouchState();
+                break;
+            case State.DownJump:
+                UpdateDownJumpState();
                 break;
             default:
                 break;
@@ -279,6 +325,14 @@ public class PlayerController : MonoBehaviour
                 _col.offset = _colOffsetOrigin;
                 _col.size = _colSizeOrigin;
                 break;
+            case State.Crouch:
+                _crouchState = CrouchState.Idle;
+                _col.offset = _colOffsetOrigin;
+                _col.size = _colSizeOrigin;
+                break;
+            case State.DownJump:
+                _downJumpState = DownJumpState.Idle;
+                break;
             default:
                 break;
         }
@@ -308,6 +362,14 @@ public class PlayerController : MonoBehaviour
                 _slideState = SlideState.Prepare;
                 _col.offset = _colOffsetCrouch;
                 _col.size = _colSizeCrouch;
+                break;
+            case State.Crouch:
+                _crouchState = CrouchState.Prepare;
+                _col.offset = _colOffsetCrouch;
+                _col.size = _colSizeCrouch;
+                break;
+            case State.DownJump:
+                _downJumpState = DownJumpState.Prepare;
                 break;
             default:
                 break;
@@ -540,6 +602,71 @@ public class PlayerController : MonoBehaviour
                     _rb.velocity = Vector2.right * _direction * _moveSpeed;
                 }
                 _animationTimer -= Time.deltaTime;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void UpdateCrouchState()
+    {
+        switch (_crouchState)
+        {
+            case CrouchState.Idle:
+                break;
+            case CrouchState.Prepare:
+                _move.x = 0;
+                isMovable = false;
+                isDirenctionChangable = true;
+                _animator.Play("Crouch");
+                _crouchState = CrouchState.OnAction;
+                break;
+            case CrouchState.Casting:
+                break;
+            case CrouchState.OnAction:
+                if(Input.GetKeyUp(KeyCode.DownArrow))
+                {
+                    ChangeState(State.Idle);
+                }
+
+                break;
+            case CrouchState.Finish:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void UpdateDownJumpState()
+    {
+        switch (_downJumpState)
+        {
+            case DownJumpState.Idle:
+                break;
+            case DownJumpState.Prepare:
+                isMovable = false;
+                isDirenctionChangable = true;
+                _animator.Play("Jump");
+                _rb.velocity = new Vector2(_rb.velocity.x, 0.0f);
+                _groundDetecter.IgnoreLastGround();
+                _downJumpState++;
+                break;
+            case DownJumpState.Casting:
+                _rb.velocity = new Vector2(_rb.velocity.x, 0.0f);
+                _rb.AddForce(Vector2.up * _downJumpForce, ForceMode2D.Impulse);
+                _downJumpState++;
+                break;
+            case DownJumpState.OnAction:
+                if(_rb.velocity.y < 0.0f)
+                {
+                    _animator.Play("Fall");
+                }
+                if(_groundDetecter.isIgnoringGround || _groundDetecter.isGroundChanged)
+                {
+                    ChangeState(State.Idle);
+                }
+                break;
+            case DownJumpState.Finish:
                 break;
             default:
                 break;
