a91f049dadeaa50097b97b1b41141b8fd1f40a5c
Hamm <master19588@gmail.com>
2022-09-13 21:52:17 +0900

    TowerDefense - Start of Project, OnMouse Event, SCRIPTABLE OBJECT, Node
    SCRIPTABLE OBJECT IS GOOD FOR BALACING SOMETHING
    satt

---
+++ TowerDefense/Assets/02Scripts/TowerInfo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TowerTypes
+{
+    MachineGun,
+    RocketLauncher,
+    Laser
+}
+
+[CreateAssetMenu(fileName = "TowerInfo", menuName = "TowerDefense/Create TowerInfo")]
+public class TowerInfo : ScriptableObject
+{
+    public TowerTypes type;
+    public int upgradeLevel;
+    public int buildPrice;
+    public int sellPrice;
+    public Sprite icon;
+
+}
