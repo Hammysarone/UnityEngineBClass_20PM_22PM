ebf19272bea0af27a8be8d0e41adfe723fc17bc3
Hamm <master19588@gmail.com>
2022-09-16 22:18:38 +0900

    TowerDefense - Upgrade System, Event
    -satt

---
+++ TowerDefense/Assets/02Scripts/LevelInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelInfo", menuName = "TowerDefense/LevelInfo")]
+public class LevelInfo : ScriptableObject
+{
+    public int lifeInit;
+    public int moenyInit;
+}
